40382a5689d590403f58f4e1eba5327c8ae30318
mansur ashrapov <fallinabbys@gmail.com>
2023-12-26 18:39:15 +0500

    b

--- app/core/config.py
+++ app/core/config.py
@@ -3,12 +3,10 @@ from datetime import datetime, timedelta
 
 
 class AppSettings(BaseSettings):
-    secret_key: str = (
-        "0lCU8ghbHhKCKFBw3UnVlQUyHSXOIKfZN2pdlUoDRkpY6TM6kleGveuIetQo9zS1"
-    )
     access_token_expires: timedelta = timedelta(days=1)
     algorithm: str = "HS256"
 
+    secret_key: str
     database_url: str 
-    
+
 config = AppSettings()
